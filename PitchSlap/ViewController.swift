2a5c840a64bfc8a1ab10747af46f4641c8edbcb4
Neamah Hussein <neamahhs@gmail.com>
2015-08-05 22:31:42 -0700

    First commit

--- PitchSlap/ViewController.swift
+++ PitchSlap/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var recordingNow: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,20 @@ class ViewController: UIViewController {
     }
 
 
+    //IB Action means it's an Interfact builder, which means it's
+    //linked to the storyboard or something similar
+    @IBAction func recordAudio(sender: UIButton) {
+        
+        //TODO: Record the audio
+        
+        recordingNow.hidden = false
+        stopButton.hidden = false
+        println("UI Button hit");
+    }
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        //TODO: Stop recording
+        recordingNow.hidden = true
+    }
 }
 
